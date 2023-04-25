6556f2ece1f933146fa4b89e559316b48d4316ce
kjl001 <kjl001@ucsd.edu>
2023-04-25 13:59:25 -0700

    try general echo py

--- cgi-bin/py-general-request-echo.py
+++ cgi-bin/py-general-request-echo.py
@@ -7,12 +7,15 @@ print("Content-Type: text/html;charset=utf-8")
 print ("Content-type:text/html\r\n")
 print("<!DOCTYPE html>")
 print("<html>")
-print("<head><title>POST Request Echo</title></head>")
-
+print("<head><title>General Request Echo</title><title>")
 print("<body>")
-print("<h1 align=center>POST Request Echo</h1>")
+print("<h1 align=center>General Request Echo</h1>")
 print("<hr>")
 
+print("<p><b>Request Method: </b>" + os.environ["REQUEST_METHOD"] + "</p><br><br>")
+print("<p><b>Protocol: </b>" + os.environ["SERVER_PROTOCOL"] + "</p><br><br>")
+print("<p><b>Query String: </b>" + os.environ["QUERY_STRING"] + "</p>")
+print("<p><b>Message Body: </b></p>")
 url = "https://kjl135.site/cgi-bin/py-post.echo.py"
 x = requests.post(url)
 

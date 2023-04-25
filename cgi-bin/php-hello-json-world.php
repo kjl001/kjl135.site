a37c453debae732acedff28e548e9d81cf28b6d0
kjl001 <kjl001@ucsd.edu>
2023-04-24 21:57:20 -0700

    add php json

---
+++ cgi-bin/php-hello-json-world.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+	<head>
+		<title>Hello PHP CGI World</title>
+	</head>
+
+	<body>
+		<?php
+			$ip = $_SERVER['REMOTE_ADDR'];
+			$date = date("m-d-Y h:i:sa");
+			$out = array("IP" => $ip, "heading": "Hello World", "time": $date, "title": "Hello PHP CGI World");
+
+			echo json_encode($out);
+		?>
+	</body>

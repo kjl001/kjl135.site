73f9e02148cb454f7041ded092025033d13d5f08
kjl001 <kjl001@ucsd.edu>
2023-04-24 21:21:55 -0700

    add php html world

---
+++ cgi-bin/php-html-world.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<head>
+	<title>Hello PHP CGI World</title>
+</head>
+
+<body>
+
+<h1 align=center>Hello HTML World!</h1>
+<hr>
+
+<?php
+	echo "Hello World"
+	echo "The program was run at: " . date("m-d-Y h:i:sa")
+	echo "Your current IP Address is: " . .$_SERVER['REMOTE_ADDR']
+?>
+
+</body>
+</html>

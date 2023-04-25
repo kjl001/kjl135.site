5aa299b7c447244706e2a2af3bf5fef8e60fe420
kjl001 <kjl001@ucsd.edu>
2023-04-25 12:36:33 -0700

    try adding cache control

--- cgi-bin/py-hello-html-world.py
+++ cgi-bin/py-hello-html-world.py
@@ -9,7 +9,7 @@ cgitb.enable()
 today = date.today()
 out = today.strftime("%B %d, %Y")
 
-print("Cache-Control: no-cache\n")
+print("Cache-Control: no-cache")
 print("Content-Type: text/html;charset=utf-8")
 print ("Content-type:text/html\r\n")
 print("<html>")

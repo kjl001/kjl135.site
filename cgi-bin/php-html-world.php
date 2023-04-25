2b8c92d5f28b41b3b915651dd45155588d472c33
kjl001 <kjl001@ucsd.edu>
2023-04-24 22:23:04 -0700

    forgot semicolon

--- cgi-bin/php-html-world.php
+++ cgi-bin/php-html-world.php
@@ -10,7 +10,7 @@
 
 <?php
 	echo "Hello World<br><br>";
-	echo "This page was generated with the PHP programming language<br><br>"
+	echo "This page was generated with the PHP programming language<br><br>";
 	echo "The program was run at: " . date("m-d-Y h:i:sa") . "<br><br>";
 	echo "Your current IP Address is: " . $_SERVER['REMOTE_ADDR'];
 ?>

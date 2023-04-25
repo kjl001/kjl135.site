a1166d4acfb1e5017be04afedc5488b3f4b51d31
kjl001 <kjl001@ucsd.edu>
2023-04-25 14:17:05 -0700

    try py session

---
+++ cgi-bin/py-sessions-1.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+import os
+
+print("Cache-Control: no-cache")
+print("Content-Type: text/html;charset=utf-8")
+print ("Content-type:text/html\r\n")
+print("<!DOCTYPE html>")
+print("<html>")
+print("<head><title>Python Sessions</title></head>")
+print("<body>")
+print("<h1 align=center>Pythong Sessions Page 1</h1>")
+print("<hr>")
+
+url = "https://kjl135.site/cgi-bin/py-sessions-1.py"
+x = requests.post(url)
+
+print(x.text)
+print("</body>")
+print("</html>")

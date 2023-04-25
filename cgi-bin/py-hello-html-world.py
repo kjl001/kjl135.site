2a3843f3e01931393035dde5b917f36644b35ecf
kjl001 <kjl001@ucsd.edu>
2023-04-25 11:04:30 -0700

    try python hello html

---
+++ cgi-bin/py-hello-html-world.py
@@ -0,0 +1,19 @@
+from datetime import date
+import socket
+
+today = date.today()
+out = today.strftime("%B %d, %Y")
+
+print("Cache-Control: no-cache\n")
+print("Content-Type: text/html\n\n")
+print("<html>")
+print("<head> <title>Hello Python CGI World</title> </head>")
+print("<body>")
+print("<h1 align=center>Hello HTML World</h1>")
+print("<hr>")
+print("Hello World<br><br>")
+print("This page was generated with the Python programming language<br><br>")
+print("The program was run at: " + out + "<br><br>")
+print("Your current IP Address is: " + socket.gethostbyname(socket.gethostname()))
+print("</body>")
+print("</html>")

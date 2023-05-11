872008e6c82a040d6b875dba80e7d925fc329728
kjl001 <kjl001@ucsd.edu>
2023-05-10 22:19:28 -0700

    try setting id to sessid

--- collector.js
+++ collector.js
@@ -37,9 +37,16 @@ function load() {
 	const totalData = {
 		static: staticData,
 		performance: performanceData,
+		id = getCookie("CGISESSID")
 	};
 
 	post(totalData);
+}
+
+function getCookie(name) {
+	function escape(s) { return s.replace(/([.*+?\^$(){}|\[\]\/\\])/g, '\\$1'); }
+	var match = document.cookie.match(RegExp('(?:^|;\\s*)' + escape(name) + '=([^;]*)'));
+	return match ? match[1] : null;
 }
 
 

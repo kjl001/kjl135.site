d096ead3bbe9be045f290b4af3cd9fd075450aa1
kjl001 <kjl001@ucsd.edu>
2023-05-12 16:50:01 -0700

    change localhost to 127

--- server.js
+++ server.js
@@ -1,53 +1,8 @@
-/*const mysql = require('mysql');
-const express = require('express');
-
-const db = mysql.createConnection({
-	host: "localhost",
-	user: "kjl001",
-	password: "Chk7!3Slct",
-	database: "kjl135"
-});
-
-db.connect((err) => {
-	if (err) throw err;
-	console.log("Connected!");
-});
-
-const app = express();
-app.use(express.json());
-
-app.post('/static', (req, res) => {
-	const data = req.body;
-	let sql = `INSERT INTO static VALUES (${data['id']}, ${data['userAgent']}, ${data['userLanguage']}, ${data['cookieEnabled']}, ${data['jsEnabled']}, ${data['imgEnabled']}, ${data['cssEnabled']}, ${data['windowWidth']}, ${data['windowHeight']});`;
-	db.query(sql, (err) => {
-		if (err) {
-			throw err;
-		}
-		res.send('Static Data Inserted');
-	});
-});
-
-app.get('/static', (req, res) => {
-	let sql = 'SELECT * FROM static';
-	db.query(sql, (err, results) => {
-		if (err) {
-			throw err;
-		}
-		res.send('Retrieving Static Data');
-		console.log(results);
-	});
-});
-
-app.listen(3000, () => {
-	console.log('Server started on port 3000');
-});
-*/
-
 import mysql from 'mysql2';
 import express from 'express';
 
 const pool = mysql.createPool({
-	host: 'localhost',
+	host: '127.0.0.1',
 	user: 'kjl001',
 	password: '',
 	database: 'kjl135'

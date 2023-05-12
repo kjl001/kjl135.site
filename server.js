4f3dab3010e18d42212cc1b24b09f9e78d087996
kjl001 <kjl001@ucsd.edu>
2023-05-11 21:50:50 -0700

    try adding post static

--- server.js
+++ server.js
@@ -1,13 +1,29 @@
-var mysql = require('mysql');
+const mysql = require('mysql');
+const express = requore('express');
 
-var con = mysql.createConnection({
+const db = mysql.createConnection({
 	host: "localhost",
 	user: "kjl001",
 	password: "Chk7!3Slct",
-	port: "3000"
+	port: "3000",
+	database: "kjl135"
 });
 
-con.connect(function (err) {
+db.connect(function (err) {
 	if (err) throw err;
 	console.log("Connected!");
+});
+
+const app = express();
+
+app.post('/static', (req, res) => {
+	let sql = `INSERT INTO static (id, userAgent, userLanguage, cookieEnabled, jsEnabled, imgEnabled, cssEnabled, windowWidth, windowHeight) VALUES (${req.body['id']}, ${req.body['userAgent']}, ${req.body['userLanguage']}, ${req.body['cookieEnabled']}, ${req.body['jsEnabled']}, ${req.body['imgEnabled']}, ${req.body['cssEnabled']}, ${req.body[windowWidth]}, ${req.body[windowHeight]})`;
+	db.query(sql, err => {
+		if (error) throw err
+		res.send('Static Data Inserted');
+	});
+});
+
+app.listen('3000', () => {
+	console.log('Server started on port 3000');
 });

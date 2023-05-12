33de0e001b8362e6887c884069ca6f64df78d96d
kjl001 <kjl001@ucsd.edu>
2023-05-12 00:59:20 +0000

    update server and db

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 var mysql = require('mysql');
 
 var con = mysql.createConnection({
-	host: "localhost",
+	host: "127.0.0.1",
 	user: "kjl001",
-	password: "Chk7!3Slct"
+	password: "Chk7!3Slct",
+	port: "3000"
 });
 
 con.connect(function (err) {

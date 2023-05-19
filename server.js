d65f3c585cee9ce744f211130779b98dd993e5df
kjl001 <kjl001@ucsd.edu>
2023-05-19 12:21:30 -0700

    change pool obj

--- server.js
+++ server.js
@@ -5,8 +5,13 @@ const pool = mysql.createPool({
 	host: process.env.MYSQL_HOST,
 	user: process.env.MYSQL_USER,
 	password: process.env.MYSQL_PASSWORD,
+	port: process.env.MYSQL_PORT,
 	database: process.env.MYSQL_DATABASE,
-	port: process.env.MYSQL_PORT
+	waitForConnections: true,
+	connectionLimit: 10,
+	maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
+	idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
+	queueLimit: 0
 }).promise();
 
 /* Get All from Table */
